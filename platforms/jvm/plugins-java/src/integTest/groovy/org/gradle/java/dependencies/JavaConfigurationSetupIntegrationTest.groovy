48319ea8f387a276f81326cb1cf36915fa77551a
Reinhold Degenfellner <rdegenfellner@gradle.com>
2024-09-20 14:20:35 +0200

    Fix tests that fail because of the problem report

--- platforms/jvm/plugins-java/src/integTest/groovy/org/gradle/java/dependencies/JavaConfigurationSetupIntegrationTest.groovy
+++ platforms/jvm/plugins-java/src/integTest/groovy/org/gradle/java/dependencies/JavaConfigurationSetupIntegrationTest.groovy
@@ -168,6 +168,7 @@ class JavaConfigurationSetupIntegrationTest extends AbstractIntegrationSpec {
         if (deprecated(alternatives)) {
             executer.expectDeprecationWarning()
         }
+        args "--no-problems-report"
         if (forbidden(alternatives) || doesNotExist(alternatives)) {
             fails 'resolve'
         } else {
